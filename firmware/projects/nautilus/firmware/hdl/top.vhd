fd8c65621ae337f2ce02885a7b04c8c42ad92b96
Dave Newbold <dave.newbold@cern.ch>
2021-05-21 21:29:18 +0100

    Adding uart loopback

--- firmware/projects/nautilus/firmware/hdl/top.vhd
+++ firmware/projects/nautilus/firmware/hdl/top.vhd
@@ -5,6 +5,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+library unisim;
+use unisim.VComponents.all;
+
 entity top is port(
 		clk: in std_logic;
 		rx: in std_logic;
@@ -30,24 +33,51 @@ architecture rtl of top is
         busy_out : OUT STD_LOGIC
       );
     END COMPONENT;
+    
+    signal clki: std_logic;
+    signal d: std_logic_vector(7 downto 0);
+    signal v: std_logic;
 
 begin
 	
-	tx <= rx;
 	ramcen <= '1'; -- Turn off the SRAM
 	qspi_cs <= '1'; -- Turn off the flash
 
-    xadc : xadc_wiz_0
-      PORT MAP (
-        vp_in => '0',
-        vn_in => '0',
-        user_temp_alarm_out => htrn,
-        channel_out => open,
-        eoc_out => open,
-        alarm_out => open,
-        eos_out => open,
-        busy_out => open
-      );
-    
-end rtl;
+	xadc : xadc_wiz_0
+		PORT MAP (
+			vp_in => '0',
+			vn_in => '0',
+			user_temp_alarm_out => htrn,
+			channel_out => open,
+			eoc_out => open,
+			alarm_out => open,
+			eos_out => open,
+			busy_out => open
+		);
+		
+	bufg: BUFG
+		port map(
+			i => clk,
+			o => clki
+		);
+		
+	uart: work.uart
+		generic map(
+			CLK_FREQ => 12e6,
+			BAUD_RATE => 9600
+		)
+		port map(
+			clk => clki,
+			rst => '0',
+			uart_txd => tx,
+			uart_rxd => rx,
+			din => d,
+			din_vld => v,
+			din_rdy => open,
+			dout => d,
+			dout_vld => v,
+			frame_error => open,
+			parity_error => open
+		);	
 
+end rtl;
